0f5a734d0c70dec206aefbf166d37ae3b699dd1f
knothhe <guanglaihe@gmail.com>
2019-01-27 15:04:42 +0800

    update 33, reference approache 1 of solution

--- LeetCode/33.search-in-rotated-sorted-array.cpp
+++ LeetCode/33.search-in-rotated-sorted-array.cpp
@@ -37,47 +37,67 @@
  * Output: -1
  * 
  */
+
+/*
+ * reference: https://leetcode.com/problems/search-in-rotated-sorted-array/solution/
+ */
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
         int len = nums.size();
-        int left = 0, right = len-1;
-        int left_end = -1, right_begin = 0;
 
-        while (left <= right) {
-            int mid = left + (right - left) / 2;
-            if (mid-1 >= 0 && nums[mid-1] > nums[mid]) {
-                left_end = mid-1;
-                right_begin = mid;
-                break;
-            }
-            if (mid+1 < len && nums[mid] > nums[mid+1]) {
-                left_end = mid;
-                right_begin = mid+1;
-                break;
-            }
-            if (nums[mid] > nums[0]) {
-                left = mid+1;
+        if (len == 0) return -1;
+        if (len == 1) return target == nums[0] ? 0 : -1;
+
+        int rotatedIndex = findRotatedIndex(nums, 0, len-1);
+
+        if (nums[rotatedIndex] == target) return rotatedIndex;
+        else {
+            if (rotatedIndex == 0) {
+                return binarySearch(nums, 0, len-1, target);
+            } else if (target < nums[0])  {
+                return binarySearch(nums, rotatedIndex, len-1, target);
             } else {
-                right = mid-1;
+                return binarySearch(nums, 0, rotatedIndex-1, target);
             }
         }
-        int res1 = binarySearch(nums, 0, left_end, target);
-        int res2 = binarySearch(nums, right_begin, nums.size()-1, target);
-
-        if (res1 != -1) return res1;
-        if (res2 != -1) return res2;
+        
         return -1;
     }
 
 private:
+    int findRotatedIndex(vector<int>& nums, int left, int right) {
+        if (nums[left] < nums[right]) {
+            return 0;
+        }
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > nums[mid+1]) {
+                return mid+1;
+            } else {
+                if (nums[mid] < nums[left]) {
+                    right = mid-1;
+                } else {
+                    left = mid+1;
+                }
+            }
+        }
+        
+        return 0;
+    }
+
     int binarySearch(vector<int>& nums, int left, int right, int target) {
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            int midNum = nums[mid];
-            if (target < midNum) right = mid-1;
-            else if (target > midNum) left = mid+1;
-            else return mid;
+            if (nums[mid] == target) return mid;
+            else {
+                if (nums[mid] < target) {
+                    left = mid+1;
+                } else {
+                    right = mid-1;
+                }
+            }
         }
 
         return -1;
