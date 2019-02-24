bec834684b42caf35416fbbc3740a27b1c8b8f92
knothhe <guanglaihe@gmail.com>
2019-02-24 15:59:51 +0800

    update 126, reference discussion

--- LeetCode/126.word-ladder-ii.cpp
+++ LeetCode/126.word-ladder-ii.cpp
@@ -61,109 +61,94 @@
  * 
  * 
  * 
- * 
+ * reference: https://leetcode.com/problems/word-ladder-ii/discuss/40475/My-concise-JAVA-solution-based-on-BFS-and-DFS
  * 
  */
 class Solution {
 public:
     vector<vector<string>> findLadders(string beginWord, string endWord, vector<string>& wordList) {
-        vector<vector<int>> next(wordList.size(), vector<int> { });
-        // begin: index of endWord
-        // end: index of beginWord
-        int begin = -1, end = -1;
-        for (int i = 0; i < wordList.size(); i++) {
-            if (wordList[i] == beginWord) {
-                end = i;
-            }
-        }
-        for (int i = 0; i < wordList.size(); i++) {
-            if (end == -1 && isNext(wordList[i], beginWord)) {
-                next[i].push_back(-1);
-            }
-            if (wordList[i] == endWord) {
-                begin = i;
-            } 
-            for (int j = i+1; j < wordList.size(); j++) {
-                if (isNext(wordList[i], wordList[j])) {
-                    next[i].push_back(j);
-                    next[j].push_back(i);
-                }
-            }
-        }
-        if (begin == -1) return vector<vector<string>> { };
-        vector<bool> visited(wordList.size(), false);
-        int minLen = bfs(next, visited, begin, end);
+        unordered_set<string> dict(wordList.begin(), wordList.end());
+        unordered_map<string, vector<string>> nodeNeighbors;
+        unordered_map<string, int> distance;
         vector<vector<string>> result;
-        vector<string> oneResult { endWord };
-        for (int i = 0; i < visited.size(); i++) {
-            visited[i] = false;
-        }
-        dfs(next, wordList, visited, result, oneResult, begin, beginWord, minLen);
-        for (auto &vec : result) {
-            reverse(vec.begin(), vec.end());
-        }
+        vector<string> solution;
+
+        dict.insert(beginWord);
+        bfs(beginWord, endWord, dict, nodeNeighbors, distance);
+        dfs(beginWord, endWord, dict, nodeNeighbors, distance, solution, result);
+
         return result;
     }
 
 private:
-    bool isNext(string word, string next) {
-        int count = 0;
-        int len = min(word.size(), next.size());
-        for (int i = 0; i < len; i++) {
-            if (word[i] != next[i]) {
-                count++;
+    void bfs(string start, string end, unordered_set<string> &dict, 
+    unordered_map<string, vector<string>> &nodeNeighbors,
+    unordered_map<string, int> &distance) {
+        queue<string> que;
+        que.push(start);
+        distance[start] = 0;
+
+        while (!que.empty()) {
+            int count = que.size();
+            bool foundEnd = false;
+            for (int i = 0; i < count; i++) {
+                string cur = que.front();
+                que.pop();
+                int curDistance = distance[cur];
+                vector<string> neighbors = getNeighbors(cur, dict);
+                nodeNeighbors[cur] = neighbors;
+
+                for (const auto &neighbor : neighbors) {
+                    if (distance.find(neighbor) == distance.end()) {
+                        distance[neighbor] = curDistance + 1;
+                        if (end == neighbor) {
+                            foundEnd = true;
+                        } else {
+                            que.push(neighbor);
+                        }
+                    }
+                }
             }
+
+            if (foundEnd) break;
         }
-        return count == 1;
     }
 
-    int bfs(vector<vector<int>> &next, vector<bool>& visited, int begin, int end) {
-        queue<int> qu;
-        qu.push(begin);
-        visited[begin] = true;
-        vector<int> distTo(visited.size(), 0);
-        distTo[begin] = 1;
-        int front;
+    vector<string> getNeighbors(string node, unordered_set<string> &dict) {
+        vector<string> result;
+        vector<char> chs(node.begin(), node.end());
+        
+        for (char ch = 'a'; ch <= 'z'; ch++) {
+            for (int i = 0; i < chs.size(); i++) {
+                if (chs[i] == ch) continue;
+                char ori_ch = chs[i];
+                chs[i] = ch;
+                string temp_str(chs.begin(), chs.end());
+                if (dict.find(temp_str) != dict.end()) {
+                    result.push_back(temp_str);
+                }
+                chs[i] = ori_ch;
+            }
+        }
 
-        while(!qu.empty()) {
-            front = qu.front();
-            qu.pop();
-            for (const int &n : next[front]) {
-                if (n == -1) {
-                    break;
-                } else if (!visited[n]) {
-                    qu.push(n);
-                    visited[n] = true; 
-                    distTo[n] = distTo[front]+1;
+        return result;
+    }
+
+    void dfs(string cur, string end, unordered_set<string> &dict,
+    unordered_map<string, vector<string>> &nodeNeighbors,
+    unordered_map<string, int> &distance, vector<string> &solution,
+    vector<vector<string>> &result) {
+        solution.push_back(cur);
+        if (end == cur) {
+            result.push_back(solution);
+        } else {
+            for (string next : nodeNeighbors[cur]) {
+                if (distance.find(next) != distance.end() && 
+                    distance[next] == distance[cur] + 1) {
+                    dfs(next, end, dict, nodeNeighbors, distance, solution, result);
                 }
             }
         }
-
-        return (end == -1) ? distTo[front]+1 : distTo[end];
-    }
-
-    void dfs(vector<vector<int>> &next, vector<string> &wordList, vector<bool> &visited,
-             vector<vector<string>> &result, vector<string> &oneResult, int index,
-             const string &target, int minLen) {
-         if (oneResult.size() == minLen) {
-            if (index == -1 || oneResult.back() == target) {
-                result.push_back(oneResult);
-            } 
-            return;
-        }
-
-        for (const int &n : next[index]) {
-            if (n == -1) {
-                oneResult.push_back(target);
-                dfs(next, wordList, visited, result, oneResult, -1, target, minLen);
-                oneResult.pop_back();
-            } else if (!visited[n]) {
-                visited[n] = true;
-                oneResult.push_back(wordList[n]);
-                dfs(next, wordList, visited, result, oneResult, n, target, minLen);
-                oneResult.pop_back();
-                visited[n] = false;
-            }
-        }
+        solution.pop_back();
     }
 };
