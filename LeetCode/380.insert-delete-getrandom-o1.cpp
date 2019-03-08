efd6c9b040ec908081e8e173be10d1b1294272cb
knothhe <guanglaihe@gmail.com>
2019-03-08 09:59:09 +0800

    add 380, reference discussion

---
+++ LeetCode/380.insert-delete-getrandom-o1.cpp
@@ -0,0 +1,96 @@
+/*
+ * @lc app=leetcode id=380 lang=cpp
+ *
+ * [380] Insert Delete GetRandom O(1)
+ *
+ * https://leetcode.com/problems/insert-delete-getrandom-o1/description/
+ *
+ * algorithms
+ * Medium (42.02%)
+ * Total Accepted:    99.8K
+ * Total Submissions: 237.6K
+ * Testcase Example:  '["RandomizedSet","insert","remove","insert","getRandom","remove","insert","getRandom"]\n[[],[1],[2],[2],[],[1],[2],[]]'
+ *
+ * Design a data structure that supports all following operations in average
+ * O(1) time.
+ * 
+ * 
+ * 
+ * insert(val): Inserts an item val to the set if not already present.
+ * remove(val): Removes an item val from the set if present.
+ * getRandom: Returns a random element from current set of elements. Each
+ * element must have the same probability of being returned.
+ * 
+ * 
+ * 
+ * Example:
+ * 
+ * // Init an empty set.
+ * RandomizedSet randomSet = new RandomizedSet();
+ * 
+ * // Inserts 1 to the set. Returns true as 1 was inserted successfully.
+ * randomSet.insert(1);
+ * 
+ * // Returns false as 2 does not exist in the set.
+ * randomSet.remove(2);
+ * 
+ * // Inserts 2 to the set, returns true. Set now contains [1,2].
+ * randomSet.insert(2);
+ * 
+ * // getRandom should return either 1 or 2 randomly.
+ * randomSet.getRandom();
+ * 
+ * // Removes 1 from the set, returns true. Set now contains [2].
+ * randomSet.remove(1);
+ * 
+ * // 2 was already in the set, so return false.
+ * randomSet.insert(2);
+ * 
+ * // Since 2 is the only number in the set, getRandom always return 2.
+ * randomSet.getRandom();
+ * 
+ * reference: https://leetcode.com/problems/insert-delete-getrandom-o1/discuss/85422/AC-C%2B%2B-Solution.-Unordered_map-%2B-Vector
+ * 
+ */
+class RandomizedSet {
+public:
+    /** Initialize your data structure here. */
+    RandomizedSet() {
+        
+    }
+    
+    /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
+    bool insert(int val) {
+        if (map.find(val) != map.end()) return false;
+        nums.push_back(val);
+        map[val] = nums.size()-1;
+        return true;
+    }
+    
+    /** Removes a value from the set. Returns true if the set contained the specified element. */
+    bool remove(int val) {
+        if (map.find(val) == map.end()) return false;
+        nums[map[val]] = nums.back();
+        map[nums.back()] = map[val];
+        nums.pop_back();
+        map.erase(val);
+        return true;
+    }
+    
+    /** Get a random element from the set. */
+    int getRandom() {
+        return nums[rand() % nums.size()];
+    }
+
+private:
+    unordered_map<int, int> map;
+    vector<int> nums;
+};
+
+/**
+ * Your RandomizedSet object will be instantiated and called as such:
+ * RandomizedSet obj = new RandomizedSet();
+ * bool param_1 = obj.insert(val);
+ * bool param_2 = obj.remove(val);
+ * int param_3 = obj.getRandom();
+ */
