fc8cc35220c2d18944d69046ba5ba1dba0f6c599
knothhe <guanglaihe@gmail.com>
2019-04-18 18:16:20 +0800

    update 560, use preSum

--- LeetCode/560.subarray-sum-equals-k.cpp
+++ LeetCode/560.subarray-sum-equals-k.cpp
@@ -35,15 +35,14 @@ public:
     int subarraySum(vector<int>& nums, int k) {
         int cnt = 0, sum = 0;
         unordered_map<int, int> dict;
-        dict.insert({0, 1});
-        for (int i = 0; i < nums.size(); ++i) {
-            sum += nums[i];
+        dict[0] = 1;
+        for (const auto &n : nums) {
+            sum += n;
             if (dict.find(sum-k) != dict.end()) {
                 cnt += dict[sum-k];
             }
             ++dict[sum];
         }
-
         return cnt;
     }
 };
