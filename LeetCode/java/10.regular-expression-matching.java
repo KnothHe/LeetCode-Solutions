75f268b3b132b995ef9891a6c8408631047822b1
knothhe <guanglaihe@gmail.com>
2019-06-02 10:35:00 +0800

    add 10, reference top-bottom dp solution

--- LeetCode/java/10.regular-expression-matching.java
+++ LeetCode/java/10.regular-expression-matching.java
@@ -81,8 +81,46 @@
  *
  *
  */
+
+enum Result {
+    TRUE, FALSE
+}
+
 class Solution {
+    private Result[][] memo;
+
     public boolean isMatch(String s, String p) {
+        memo = new Result[s.length()+1][p.length()+1];
+        return dp(0, 0, s, p);
+    }
+
+    private boolean dp(int i, int j, String s, String p) {
+        if (memo[i][j] != null) {
+            return memo[i][j] == Result.TRUE;
+        }
+
+        boolean ans;
+
+        if (j == p.length()) {
+            ans = (i == s.length());
+        } else {
+            boolean first_match = (i < s.length() &&
+                    (p.charAt(j) == s.charAt(i) ||
+                     p.charAt(j) == '.'));
+
+            if (j+1 < p.length() && (p.charAt(j+1) == '*')) {
+                ans = dp(i, j+2, s, p) ||
+                    (first_match && dp(i+1, j, s, p));
+            } else {
+                ans = first_match && dp(i+1, j+1, s, p);
+            }
+        }
+
+        memo[i][j] = (ans ? Result.TRUE : Result.FALSE);
+        return ans;
+    }
+
+    private boolean solve1(String s, String p) {
         if (p.isEmpty()) return s.isEmpty();
         boolean first_match = (!s.isEmpty() &&
                 (p.charAt(0) == s.charAt(0) ||
