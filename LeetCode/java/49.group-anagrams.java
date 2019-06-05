e70695dbc23e61d1d4dd2d566ee56b96fe394ae4
knothhe <guanglaihe@gmail.com>
2019-06-05 20:40:25 +0800

    update 49(java), use count sort

--- LeetCode/java/49.group-anagrams.java
+++ LeetCode/java/49.group-anagrams.java
@@ -35,7 +35,6 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
         Map<String, List<String>> map = new HashMap<>();
-        List<List<String>> result = new ArrayList<>();
 
         for (String s : strs) {
             char[] chs = s.toCharArray();
@@ -48,5 +47,31 @@ class Solution {
         }
 
         return new ArrayList(map.values());
+    }
+
+    public List<List<String>> solve1(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+        int[] count = new int[26];
+
+        for (String s : strs) {
+            Arrays.fill(count, 0);
+
+            for (char c : s.toCharArray()) {
+                count[c-'a']++;
+            }
+            
+            StringBuilder sb = new StringBuilder();
+            for (int i : count) {
+                sb.append("#").append(i);
+            }
+            
+            String key = sb.toString();
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+            map.get(key).add(s);
+        }
+
+        return new ArrayList<>(map.values());
     }
 }
