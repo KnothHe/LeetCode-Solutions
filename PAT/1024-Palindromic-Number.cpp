48b9ae579fce6f483dd46f5920ad5af8f924a560
knothhe <guanglaihe@gmail.com>
2019-06-27 15:15:29 +0800

    PAT:A Update 1024

--- PAT/1024-Palindromic-Number.cpp
+++ PAT/1024-Palindromic-Number.cpp
@@ -1,56 +1,34 @@
+/*
+ * Reference:
+ *  https://www.liuchuo.net/archives/2329
+ */
 #include <iostream>
+#include <algorithm>
 #include <string>
 using namespace std;
-inline int charToInt(char c) {
-    return c - '0';
-}
-inline char intToChar(int n) {
-    return n + '0';
-}
-string revStr(string input) {
-    string res;
-    for (int i = input.size() - 1; i >= 0; i--) {
-        res += input[i];
-    }
-    return res;
-}
-string addStr(string n1, string n2) {
-    string res;
+string s;
+void add(string t) {
     int carry = 0;
-    int len1 = n1.size();
-    int len2 = n2.size();
-    for (int i1 = len1 - 1, i2 = len2 - 1; i1 >= 0 || i2 >= 0; ) {
-        int a1 = i1 >= 0 ? charToInt(n1[i1]) : 0;
-        int a2 = i2 >= 0 ? charToInt(n2[i2]) : 0;
-        int a = a1 + a2 + carry;
-        res = intToChar(a % 10) + res; 
-        carry = a / 10;
-        i1--; i2--;
+    for (int i = 0; i < t.size(); i++) {
+        s[i] += t[i] - '0' + carry;
+        carry = 0;
+        if (s[i] > '9') {
+            s[i] -= 10;
+            carry = 1;
+        }
     }
-    if (carry != 0) {
-        res = intToChar(carry) + res;
-    }
-    return res;
-}
-bool isPaild(string n) {
-    for (int i = 0; i < n.size() / 2; i++) {
-        if (n[i] != n[n.size() - i - 1])
-            return false;
-    }
-    return true;
+    if (carry) s += '1';
+    reverse(s.begin(), s.end());
 }
 int main() {
     int step, K;
-    string num;
-    cin >> num >> K;
+    cin >> s >> K;
     for (step = 0; step < K; step++) {
-        if (isPaild(num)) {
-            cout << num << '\n' << step << endl;
-            return 0;
-        }
-        num = addStr(num, revStr(num));
+        string t(s);
+        reverse(t.begin(), t.end());
+        if (t == s) break;
+        add(t);
     }
-    cout << num << '\n' << step << endl;
+    cout << s << '\n' << step << endl;
     return 0;
 }
-
