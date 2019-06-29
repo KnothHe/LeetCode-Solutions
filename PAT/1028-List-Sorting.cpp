a23cf231935cae254d4eb22034308753ea62ebed
knothhe <guanglaihe@gmail.com>
2019-06-29 22:52:34 +0800

    PAT:A Update 1028, use lambda capture

--- PAT/1028-List-Sorting.cpp
+++ PAT/1028-List-Sorting.cpp
@@ -3,35 +3,28 @@
 #include <vector>
 using namespace std;
 struct Record {
-    string id, name, score;
+    string id, name;
+    int score;
 } record;
-int N, C;
 int main() {
+    int N, C;
     cin >> N >> C;
     vector<Record> records(N);
     for (int i = 0; i < N; i++) {
         cin >> record.id >> record.name >> record.score;
         records[i] = record;
     }
-    switch (C) {
-        case 1 : sort(records.begin(), records.end(), [](auto &lhs, auto &rhs) {
-                         return lhs.id < rhs.id;
-                         });
-                 break;
-        case 2 : sort(records.begin(), records.end(), [](auto &lhs, auto &rhs) {
-                         return lhs.name != rhs.name ? lhs.name <= rhs.name :
-                            lhs.id < rhs.id; 
-                         });
-                 break;
-        case 3 : sort(records.begin(), records.end(), [](auto &lhs, auto &rhs) {
-                         return lhs.score != rhs.score ? lhs.score <= rhs.score :
-                            lhs.id < rhs.id;
-                         });
-                 break;
-        default : break;
-    }
+    sort(records.begin(), records.end(), [C](auto &lhs, auto &rhs) {
+            switch (C) {
+                case 2 : return lhs.name != rhs.name ? lhs.name <= rhs.name :
+                    lhs.id < rhs.id;
+                case 3 : return lhs.score != rhs.score ? lhs.score <= rhs.score :
+                    lhs.id < rhs.id;
+                default : return lhs.id < rhs.id;
+            }});
     for (auto &r : records) {
         cout << r.id << " " << r.name << " " << r.score << endl;
     }
     return 0;
 }
+
