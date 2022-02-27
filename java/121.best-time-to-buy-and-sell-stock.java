19d17335ee3a61bf42f03a64b8168cc6b1634f64
guanglai <guanglai.he@weimob.com>
2022-02-28 02:44:41 +0800

    Update Mon Feb 28 02:44:41 CST 2022

--- java/121.best-time-to-buy-and-sell-stock.java
+++ java/121.best-time-to-buy-and-sell-stock.java
@@ -56,7 +56,16 @@
 // @lc code=start
 class Solution {
     public int maxProfit(int[] prices) {
-        
+        int minPrice = Integer.MAX_VALUE; int maxPrice = 0;
+
+        for (int i = 0; i < prices.length; i++) {
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else {
+                maxPrice = Math.max(maxPrice, prices[i] - minPrice);
+            }
+        }
+        return maxPrice;
     }
 }
 // @lc code=end
