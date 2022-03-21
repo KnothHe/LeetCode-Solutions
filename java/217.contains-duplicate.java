575fdbd85bc08d0ef89241431b07c7af712513db
guanglai <guanglai.he@weimob.com>
2022-03-21 10:49:01 +0800

    DONE 217

--- java/217.contains-duplicate.java
+++ java/217.contains-duplicate.java
@@ -40,7 +40,14 @@
 // @lc code=start
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        
+        Set<Integer> s = new HashSet<>();
+        for (int num : nums) {
+            if (s.contains(num)) {
+                return true;
+            }
+            s.add(num);
+        }
+        return false;
     }
 }
 // @lc code=end
